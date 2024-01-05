be69dffbe400d6861a5b73592a3fbda62866818c
Katteryne Gaona <kagaona@pol.una.py>
2024-01-05 17:04:29 -0300

    actualizacion de base

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://katty0055.github.io/postulacionV1/'
 })
